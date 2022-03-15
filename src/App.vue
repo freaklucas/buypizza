3aff632ff7ca976148ce3866b4cb8e9ba40f98d8
freaklucas <lucas.oliveira.freak@gmail.com>
2022-03-14 23:11:16 -0300

    Setup tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <div class="container">
+    <Home />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Home from "./components/home/Home.vue";
 export default {
-  name: 'App',
-
+  name: "App",
   components: {
-    HelloWorld,
+    Home,
   },
-
-  data: () => ({
-    //
-  }),
-}
+};
 </script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import { loadFonts } from "./plugins/webfontloader";
 
-loadFonts();
+import "./index.css";
 
-createApp(App).use(vuetify).mount("#app");
+createApp(App).mount("#app");
