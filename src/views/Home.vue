1935a9f2f5982c804bb95fd7823b682ae4ff8006
freaklucas <lucas.oliveira.freak@gmail.com>
2022-03-15 23:27:40 -0300

    Setup parallax

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,23 @@
 <template>
-  <div></div>
+  <div>
+    <header
+      class="
+        flex
+        items-center
+        justify-center
+        h-screen
+        mb-12
+        bg-fixed bg-center bg-cover
+        custom-img
+      "
+    >
+      <div
+        class="p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl"
+      >
+        Pizza Buy
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
