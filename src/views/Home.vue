a09bad6f7d35d5483ee91e0b0597fe94ae35e828
freaklucas <lucas.oliveira.freak@gmail.com>
2022-03-16 19:59:11 -0300

    parallax criado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,20 @@
 <template>
-  <div>
-    <header
-      class="
-        flex
-        items-center
-        justify-center
-        h-screen
-        mb-12
-        bg-fixed bg-center bg-cover
-        custom-img
-      "
-    >
-      <div
-        class="p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl"
-      >
-        Pizza Buy
-      </div>
-    </header>
+  <div
+    class="
+      w-full
+      h-full
+      flex
+      items-center
+      justify-center
+      h-screen
+      mb-12
+      bg-fixed bg-center bg-cover
+      custom-img
+    "
+  >
+    <div class="p-5 text-2xl text-white bg-purple-300 bg-opacity-50 rounded-xl">
+      Parralax inline
+    </div>
   </div>
 </template>
 
@@ -27,4 +25,7 @@ export default {
 </script>
 
 <style>
+.custom-img {
+  background-image: url("https://images.pexels.com/photos/2147491/pexels-photo-2147491.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=1706&w=1280");
+}
 </style>
