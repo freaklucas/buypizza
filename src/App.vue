065d576ec9183d3ec69e14ca575acaef77004b33
freaklucas <lucas.oliveira.freak@gmail.com>
2022-03-21 22:16:34 -0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./components/template/Header.vue";
+import Footer from "./components/template/Footer.vue";
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
   },
 };
 </script>
---
+++ src/components/template/Footer.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="max-w-2xl mx-auto">
+    <div class="flex justify-center">
+      <div class="grid grid-cols-2 gap-4 sm:gap-6 sm:grid-cols-3 center">
+        <div>
+          <h3
+            class="
+              mb-6
+              text-sm
+              font-poppins
+              text-gray-900
+              uppercase
+              dark:text-white
+              gap-8
+            "
+          >
+            Company
+          </h3>
+          <ul>
+            <li class="mb-4">
+              <a
+                href="https://flowbite.com"
+                target="_blank"
+                class="text-gray-600 hover:underline dark:text-gray-400"
+                >Flowbite</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://tailwindcss.com/"
+                target="_blank"
+                rel="nofollow"
+                class="text-gray-600 hover:underline dark:text-gray-400 mb-8"
+                >Tailwind CSS</a
+              >
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3
+            class="
+              mb-6
+              text-sm
+              font-poppins
+              text-gray-900
+              uppercase
+              dark:text-white
+            "
+          >
+            Company
+          </h3>
+          <ul>
+            <li class="mb-4">
+              <a
+                href="https://flowbite.com"
+                target="_blank"
+                class="text-gray-600 hover:underline dark:text-gray-400"
+                >Flowbite</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://tailwindcss.com/"
+                target="_blank"
+                rel="nofollow"
+                class="text-gray-600 hover:underline dark:text-gray-400 mb-8"
+                >Tailwind CSS</a
+              >
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3
+            class="
+              mb-6
+              text-sm
+              font-poppins
+              text-gray-900
+              uppercase
+              dark:text-white
+            "
+          >
+            Company
+          </h3>
+          <ul>
+            <li class="mb-4">
+              <a
+                href="https://flowbite.com"
+                target="_blank"
+                class="text-gray-600 hover:underline dark:text-gray-400"
+                >Flowbite</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://tailwindcss.com/"
+                target="_blank"
+                rel="nofollow"
+                class="text-gray-600 hover:underline dark:text-gray-400 mb-8"
+                >Tailwind CSS</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
