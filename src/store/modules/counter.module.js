a9fff410b05b03a7990cc02174bd71ab2d68ede4
freaklucas <lucas.oliveira.freak@gmail.com>
2022-04-06 17:27:12 -0300

    modulo de increment ok

---
+++ src/store/modules/counter.module.js
@@ -0,0 +1,30 @@
+export default {
+  namespaced: true,
+  state: {
+    counter: {
+      count: 1,
+    },
+  },
+  mutations: {
+    INCREMENT_COUNTER: function (state) {
+      state.counter.count = state.counter.count + 1;
+    },
+    DECREMENT_COUNTER: function (state) {
+      state.counter.count = state.counter.count - 1;
+    },
+    INCREMENT_COUNTER_BY: function (state, payload) {
+      state.counter.count = state.counter.count + payload.value;
+    },
+  },
+  actions: {
+    incrementCounter: function ({ commit }) {
+      return commit("INCREMENT_COUNTER");
+    },
+    decrementCounter: function ({ commit }) {
+      return commit("DECREMENT_COUNTER");
+    },
+    incrementCounterBy: function ({ commit }, payload) {
+      return commit("INCREMENT_COUNTER_BY", payload);
+    },
+  },
+};
