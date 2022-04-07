85be6b13a3cd7331f8905b613dce1b24c1e3c81b
freaklucas <lucas.oliveira.freak@gmail.com>
2022-04-07 00:37:00 -0300

    modulo do contador

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+
+import counterModule from "./modules/counter.module";
 
 export default createStore({
   state: {
+    counterModule: counterModule.state,
   },
-  mutations: {
-  },
-  actions: {
-  },
+  mutations: {},
+  actions: {},
   modules: {
-  }
-})
+    counterModule,
+  },
+  getters: {
+    getCounterState: function (state) {
+      return state.counterState.counter;
+    },
+  },
+});
