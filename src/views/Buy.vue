82b07c9690294f9cadb3aa1736f9eca0ccd7d150
freaklucas <lucas.oliveira.freak@gmail.com>
2022-05-29 19:10:49 -0300

    componente buy ok

--- src/views/Buy.vue
+++ src/views/Buy.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="container">
+  <div class="flex justify-center flex-col">
     <h2
       class="
+        center
+        text-center text-4xl
+        font-semibold
         dark:text-gray-400
         mb-8
+        mt-8
         hover:text- hover:text-red-300
         cursor-pointer
         hover:border-red-600
@@ -11,28 +15,35 @@
     >
       Pagamento...
     </h2>
+
+    <h2
+      class="
+        info-box
+        text-2xl
+        font-poppins
+        flex
+        justify-center
+        p-1
+        font-light
+        text-gray-500
+        ml-2
+        mb-8
+        mt-8
+      "
+    >
+      Calabresa | Calabresa Especial
+    </h2>
+    <p class="flex justify-center text-red-400 text-xl font-semibold mt-8 mb-8">
+      Total: {{ counterState.count * 55 + counterState.countEspecial * 60 }}
+    </p>
   </div>
 </template>
 
-<style>
-.container {
-  padding: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  flex-direction: wrap;
-
-  margin-bottom: 25px;
-}
-
-h2 {
-  font-size: 2.5rem;
-  text-align: center;
-  font-weight: bold;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: mapGetters({
+    counterState: "getCounterState",
+  }),
+};
+</script>
